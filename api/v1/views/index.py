9bcdfb83aea274791f267c667bf05f6ea22a4d61
abrhamadddis <abrhamaddis32@gmail.com>
2022-10-23 12:42:33 +0300

    some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,19 @@
 
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
+from models.engine import count, db_storage
 
 
 @app_views.route('/status')
 def status():
     '''return status'''
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/api/v1/stats')
+def stats():
+    my_list = {}
+    for stats in db_storage.classes:
+        num = db_storage.count(stats)
+        my_list.update({'stats': num})
+    return jsonify({my_list})
