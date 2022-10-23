11343de933f584a0e138db167da41ea3ac3e6040
Melaku Alehegn <49185991+MelakuAlehegn@users.noreply.github.com>
2022-10-23 14:45:51 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
-'''first endpoint (route) to return the status of your API'''
-
-import code
-from flask import Flask
+"""app.py to connect to API"""
+import os
 from models import storage
 from api.v1.views import app_views
-from flask import Blueprint, jsonify, make_response
-import os
+from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+
+
 @app.teardown_appcontext
-def closestorage(code):
-    '''teardown context'''
+def teardown_appcontext(code):
+    """teardown_appcontext"""
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(e):
-    '''Return 404 error'''
-    return make_response(jsonify({"error": "Not found"}), 404)
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
-    app.run(host=os.getenv("HBNB_API_HOST", '0.0.0.0'),
-            port=os.getenv('HBNB_API_PORT', '5000'), threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
+ 
